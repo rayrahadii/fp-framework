db621bb85c86aa6dd9ed7188724b1d658d0e5d0d
rayrahadii <rahadisasmita@gmail.com>
2020-05-28 23:08:21 +0700

    Fix Wess !

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -33,7 +33,7 @@
             <div class="copyright">
                 <p>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> <i class="icon-heart" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank" ></a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </p>
             </div>
